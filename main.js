1e9d08ab4cb0b9d25acca93c4d11ea8a087d2bf8
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-05-17 23:19:04 +0545

    Ft:responsive-500

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   const nav = document.querySelector("nav");
-
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 60) {
-      nav.classList.add("scrolled");
-    } else {
-      nav.classList.remove("scrolled");
-    }
+  const mobileNav = document.querySelector("nav.mobile-nav");
+  const mobileIcon = document.querySelector(".menu-icon");
+  const closeIcon = document.querySelector(
+    ".mobile-menu-container .close-icon"
+  );
+  const mobileMenuContainer = document.querySelector(".mobile-menu-container");
+  if (mobileNav) {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 60) {
+        nav.classList.add("scrolled");
+        mobileNav.classList.add("scrolled");
+      } else {
+        nav.classList.remove("scrolled");
+        mobileNav.classList.remove("scrolled");
+      }
+    });
+  }
+  mobileIcon.addEventListener("click", () => {
+    mobileMenuContainer.classList.add("active");
+  });
+  closeIcon.addEventListener("click", () => {
+    mobileMenuContainer.classList.remove("active");
   });
 });
